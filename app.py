14c7bda9d518045d0641b74ea10dd96f7db0403a
Denis <HepBoTpeTTGames@yandex.ru>
2024-10-16 15:15:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,7 @@ import pandas as pd
 import tkinter as tk
 from PIL import Image, ImageTk
 from pathlib import Path
-from math import ceil, sqrt
-# import asyncio
-# from datetime import datetime
+from math import ceil
 
 class SyncCheker:
     def __init__(self, window):
@@ -16,17 +14,20 @@ class SyncCheker:
         global min_frame
         if min_frame >= max_frame: return
 
-        min_frame += 0.2
-        for video_player in self.video_players:
-            video_player.update()
+        if is_playing:
+            min_frame += 0.2
+            for player in self.video_players: player.update()
+            print('--------------------------')
         self.window.after(speed, self.update_min_frame)
 
 
 class VideoPlayer:
     def __init__(self, window, video_source, annotation, row, col):
         self.frame_id = 0
+        self.name = int(f"{row}{col}", 2)
         self.previous_frame = None
         self.annotation_pd = annotation
+        self.is_able_to_play = True
         
         self.video_file = cv2.VideoCapture(video_source)
         self.canvas = tk.Canvas(window, width=int(self.video_file.get(cv2.CAP_PROP_FRAME_WIDTH)) / 2,
@@ -34,39 +35,35 @@ class VideoPlayer:
                                 background="black")
         self.canvas.grid(row=row, column=col)
 
-        # self.update()
-
     def update(self):
         global is_playing
         try:
             next_frame_timestamp = self.annotation_pd.iloc[self.frame_id + 1].values[0]
 
-            if (next_frame_timestamp - min_frame) // 0.1 <= 2 and is_playing:
-                # print('----------', self.name, 'НОВЫЙ')
-
+            if 0 <= ceil((next_frame_timestamp - min_frame) / 0.1) <= 2 and self.is_able_to_play:
                 ret, frame = self.video_file.read()
+                if ret is None: return ValueError()
 
-                if ret is not None:
-                    self.previous_frame = frame
+                self.previous_frame = frame
 
-                    canvas_width = self.canvas.winfo_width()
-                    canvas_height = self.canvas.winfo_height()
+                canvas_width = self.canvas.winfo_width()
+                canvas_height = self.canvas.winfo_height()
 
-                    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                    img = Image.fromarray(frame)
-                    img = img.resize((canvas_width, canvas_height))
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                img = Image.fromarray(frame)
+                img = img.resize((canvas_width, canvas_height))
 
-                    imgtk = ImageTk.PhotoImage(image=img)
+                imgtk = ImageTk.PhotoImage(image=img)
 
-                    self.canvas.create_image(0, 0, anchor=tk.NW, image=imgtk)
-                    self.canvas.imgtk = imgtk 
+                self.canvas.create_image(0, 0, anchor=tk.NW, image=imgtk)
+                self.canvas.imgtk = imgtk
+
+                print(f"Player {self.name} \t frame {self.frame_id}\t\tGOOD")
                 self.frame_id += 1
                     
 
 
-            else:
-                # print('-', self.name, "СТАРЫЙ")
-
+            elif self.previous_frame is not None:
                 canvas_width = self.canvas.winfo_width()
                 canvas_height = self.canvas.winfo_height()
 
@@ -79,19 +76,25 @@ class VideoPlayer:
                 imgtk = ImageTk.PhotoImage(image=img)
 
                 self.canvas.create_image(0, 0, anchor=tk.NW, image=imgtk)
-                self.canvas.imgtk = imgtk  # Сохраняем ссылку на изображение
+                self.canvas.imgtk = imgtk
+                print(f"Player {self.name} \t frame {self.frame_id}\t\tBAD")
+
+            else:
+                print(f"Player {self.name} \t frame {self.frame_id}\t\tNone")
+                
+
 
         except Exception as e:
             print(f"{e} : {e.__class__.__name__}")
 
             if e.__class__.__name__ == "error":
+                print(f"Player {self.name} \t frame {self.frame_id}\t\tBROKEN")
                 self.frame_id += 1
-                
 
             if e is IndexError:
                 self.video_file.set(cv2.CAP_PROP_POS_FRAMES, 0)
                 self.frame_id = 0
-                is_playing = False
+                self.is_able_to_play = False
 
 
 def read_timestamps(file_path):
@@ -126,21 +129,25 @@ def main():
     button3 = tk.Button(root, text="play / pause", command=lambda: play_pause(), background="black", foreground="white")
     button3.grid(row=3, column=1)    
 
-    cells = ceil(sqrt(len(video_paths)))
+    cells = 2
     sync = SyncCheker(root)
 
     for index, video_path in enumerate(video_paths):
         row = index // cells if index > 0 else 0
         col = index % cells if index > 0 else 0
         sync.video_players.append(VideoPlayer(root, video_path, all_timestamps[index], row, col))
-        # print('----------------', (datetime.now() - start_time).total_seconds())
     sync.update_min_frame()
 
     root.mainloop()
         
-def change_speed(speed_new):
-    global speed
-    speed = int(200//speed_new)
+def change_speed(koeff):
+    global speed, is_playing
+
+    saved_flag = is_playing
+    is_playing = False
+    speed = int(200//koeff)
+    print(f'\nupdate period:{speed}ms / {1000//speed}fps\n')
+    is_playing = saved_flag
 
 def play_pause():
     global is_playing
